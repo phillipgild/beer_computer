c867604c660486c288f14ae77560128b9cefc886
GeorgJunker <georg_junker@hotmail.com>
2026-03-17 10:58:02 +0100

    yallah

--- setup.py
+++ setup.py
@@ -70,11 +70,79 @@ def generate_mac_command_files():
 
     os.chmod(command_path, 0o755)
 
+def generate_windows_bat_files():
+    """Generate .bat files for Windows users to easily run the scripts in the repository."""
+    windows_dir = 'windows_scripts'
+    
+    # Generate the run_scanner.bat file
+    os.makedirs(windows_dir, exist_ok=True)
+
+    bat_filename = os.path.join(windows_dir, "run_scanner.bat")
+    repo_dir = os.path.abspath(os.path.dirname(__file__))
+    
+    bat_content = f"""
+    cd "{repo_dir}"
+    python3 scanner.py
+    pause
+    """
+
+    bat_path = os.path.join(repo_dir, bat_filename)
+
+    with open(bat_path, "w") as f:
+        f.write(bat_content)
+        
+    os.chmod(bat_path, 0o755)
+    
+    # Generate the run_find_prices.bat file
+    bat_filename = os.path.join(windows_dir, "run_find_prices.bat")
+    bat_content = f"""
+    cd "{repo_dir}"
+    python3 find_prices_divide_losses_equally.py
+    pause
+    """
+
+    bat_path = os.path.join(repo_dir, bat_filename)
+
+    with open(bat_path, "w") as f:
+        f.write(bat_content)
+    
+    os.chmod(bat_path, 0o755)
+
+    # Generate the generate_qr_codes.bat file
+    bat_filename = os.path.join(windows_dir, "generate_qr_codes.bat")
+    bat_content = f"""
+    cd "{repo_dir}"
+    python3 generate_qr_codes.py
+    pause
+    """
+
+    bat_path = os.path.join(repo_dir, bat_filename)
+
+    with open(bat_path, "w") as f:
+        f.write(bat_content)
+
+    os.chmod(bat_path, 0o755)
+
+    # Generate the generate_barcodes.bat file
+    bat_filename = os.path.join(windows_dir, "generate_barcodes.bat")
+    bat_content = f"""
+    cd "{repo_dir}"
+    python3 generate_barcodes.py
+    pause
+    """
+
+    bat_path = os.path.join(repo_dir, bat_filename)
+
+    with open(bat_path, "w") as f:
+        f.write(bat_content)
+
+    os.chmod(bat_path, 0o755)
 
 def main():
     """Main function."""
     # Generate the .command files for Mac users
     generate_mac_command_files()
+    generate_windows_bat_files()
 
 if __name__ == "__main__":
     main()
